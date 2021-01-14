8283671de764b16e7f0f864d5e8e253dde44a325
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-14 15:03:10 +0200

    Site version 2.0.0

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded',() => {
-
+document.addEventListener('DOMContentLoaded', () => {
+    // Tab
     const contentText = document.querySelectorAll('.tabcontent'),
-          tabClass = document.querySelectorAll('.tabheader__item'),
-          tabParent = document.querySelector('.tabheader__items');
+        tabClass = document.querySelectorAll('.tabheader__item'),
+        tabParent = document.querySelector('.tabheader__items');
 
     function tabHide() {
         contentText.forEach(value => {
@@ -22,14 +22,14 @@ document.addEventListener('DOMContentLoaded',() => {
         tabClass[i].classList.add('tabheader__item_active');
     }
 
-    tabParent.addEventListener('click',(e) => {
+    tabParent.addEventListener('click', (e) => {
         const currentTarget = e.target;
-        if(currentTarget && currentTarget.matches('div.tabheader__item')) {
-            tabClass.forEach((value,i) => {
-              if(value == currentTarget) {
-                tabHide();
-                showTab(i);
-              }
+        if (currentTarget && currentTarget.matches('div.tabheader__item')) {
+            tabClass.forEach((value, i) => {
+                if (value == currentTarget) {
+                    tabHide();
+                    showTab(i);
+                }
             });
         }
     });
@@ -37,4 +37,65 @@ document.addEventListener('DOMContentLoaded',() => {
 
     tabHide();
     showTab();
+
+
+
+    //Timer
+
+
+    const deadline = "2021-01-15";
+
+    function calcTime(endtime) {
+        
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        console.log(t);
+        return {
+            t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function helper(num) {
+        if(num < 10) {
+            return `0${num}`
+        }else {
+            return num;
+        }
+
+    }
+
+
+    function showTime(selector, endtime) {
+        const parentElement = document.querySelector(selector),
+            days = parentElement.querySelector('#days'),
+            hours = parentElement.querySelector('#hours'),
+            minutes = parentElement.querySelector('#minutes'),
+            seconds = parentElement.querySelector('#seconds');
+
+        const time = setInterval(callHide, 1000);
+
+        callHide();
+
+        function callHide() {
+            const r = calcTime(endtime);
+           
+            days.innerHTML = helper(r.days);
+            hours.innerHTML = helper(r.hours);
+            minutes.innerHTML = helper(r.minutes);
+            seconds.innerHTML = helper(r.seconds);
+
+
+            if (r.t <= 0) {
+                clearInterval(time);
+            }
+        }
+    }
+    showTime('.timer',deadline);
 });
