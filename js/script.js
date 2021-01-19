cb0250ac8334de85c4bf4a68b42fc4ca486b0516
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-19 22:01:44 +0200

    form

--- js/script.js
+++ js/script.js
@@ -102,12 +102,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     //Tabs
-
     const modal = document.querySelector('.modal');
     document.querySelector('body').addEventListener('click', (e) => {
         if (e.target && e.target.matches('[data-modal]')) {
             callModalWindow();
-
         }
     });
 
@@ -121,11 +119,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    //const result = setTimeout(callModalWindow, 5000);
+    const result = setTimeout(callModalWindow, 50000);
 
     function callModalWindow() {
-        showWindow(modal);
-        hideWindow(modal);
+        showWindow();
+        hideWindow();
         clearInterval(result);
 
     }
@@ -136,17 +134,17 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = 'hidden';
     }
 
-    function closeModal(e) {
-        e.classList.remove('show');
-        e.classList.add('hide');
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
         document.body.style.overflow = '';
     }
 
-    function hideWindow(element) {
+    function hideWindow() {
         modal.addEventListener('click', (event) => {
             const t = event.target;
             if (t && t.matches('[data-close]')) {
-                closeModal(element);
+                closeModal();
             }
         });
 
@@ -154,7 +152,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('keydown', (e) => {
         if (e.code === 'Escape' && modal.classList.contains('show')) {
-            closeModal(modal);
+            closeModal();
         }
     });
 
@@ -189,7 +187,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Menu
     class Menu {
-        constructor(src, tittle, content, price, alt,parent) {
+        constructor(src, tittle, content, price, alt, parent, ...classes) {
             this.src = src;
             this.alt = alt;
             this.tittle = tittle;
@@ -197,9 +195,9 @@ document.addEventListener('DOMContentLoaded', () => {
             this.price = price;
             this.transfer = 28;
             this.parent = document.querySelector(parent);
+            this.classes = classes;
             this.courseOfValut();
             this.addContentOfMenu();
-            
         }
 
         courseOfValut() {
@@ -207,23 +205,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         addContentOfMenu() {
-        let div = document.createElement('div');
+            let div = document.createElement('div');
 
-        
+            if (this.classes.length === 0) {
+                div.classList.add('menu__item');
+            } else {
+                this.classes.forEach(classElement => div.classList.add(classElement));
+            }
 
+            div.innerHTML = `
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.tittle}</h3>
+             <div class="menu__item-descr">${this.content}</div>
+                <div class="menu__item-divider"></div>
+             <div class="menu__item-price">
+                 <div class="menu__item-cost">Цена:</div>
+                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+              </div>
+             `;
 
-            this.parent.innerHTML += `
-        <div class = "menu__item">
-            <img src="${this.src}" alt="${this.alt}">
-            <h3 class="menu__item-subtitle">${this.tittle}</h3>
-            <div class="menu__item-descr">${this.content}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-             </div>
-        </div>
-        `;
+            this.parent.append(div);
         }
     }
 
@@ -233,26 +234,103 @@ document.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         7,
         'image',
-        '[data-content]'
-        );
+        '[data-content]',
 
-        new Menu(
-            'img/tabs/elite.jpg',
-            'Меню “Премиум”',
-            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-            9,
-            'image',
-            '[data-content]'
-            );
-        
-            new Menu(
-                'img/tabs/post.jpg',
-                'Меню "Постное"',
-                'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное  количество белков за счет тофу и импортных вегетарианских стейков.!',
-                8,
-                'image',
-                '[data-content]'
-                );
-            
-   
+    );
+
+    new Menu(
+        'img/tabs/elite.jpg',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        9,
+        'image',
+        '[data-content]',
+
+    );
+
+    new Menu(
+        'img/tabs/post.jpg',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное  количество белков за счет тофу и импортных вегетарианских стейков.!',
+        8,
+        'image',
+        '[data-content]',
+
+    );
+
+    //form
+    const forms = document.querySelectorAll('form');
+
+    forms.forEach(item => sendRequest(item));
+
+    function sendRequest(form) {
+
+        const loadMessage = {
+            load: "img/form/spinner.svg",
+            success: 'Данные успешно отправленны',
+            fail: 'Ошибка'
+        };
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+             const img = document.createElement('img');
+             img.src = loadMessage.load;
+             img.style.cssText = `
+                 display: block;
+                 margin: 0 auto;
+             `;
+            form.append(img);
+
+            const formData = new FormData(form);
+            request.setRequestHeader('Document-type', 'application/json');
+            const object = {};
+            formData.forEach((value, key) => {
+                object[key] = value;
+            });
+
+            const dataObject = JSON.stringify(object);
+
+            request.send(dataObject);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    showMessage(loadMessage.success);
+                    form.reset();
+                    img.remove();
+                } else {
+                    showMessage(loadMessage.fail);
+                }
+            });
+        });
+
+        function showMessage(message) {
+            const modalDialog = document.querySelector('.modal__dialog');
+
+            modalDialog.classList.add('hide');
+            callModalWindow();
+
+            const divModal = document.createElement('div');
+            divModal.classList.add('modal__dialog');
+
+            divModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__title">${message}</div>
+            </div>
+            `;
+
+            modal.append(divModal);
+           
+            setTimeout(() => {
+                divModal.remove();
+                modalDialog.classList.remove('hide');
+                modalDialog.classList.add('show');
+                closeModal();
+            },3000);
+        }
+    }
 });
