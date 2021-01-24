49fbb827f7970e7f8a7e8da290d7c3375aac0d36
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-24 22:26:09 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Tab
-
     const contentText = document.querySelectorAll('.tabcontent'),
         tabClass = document.querySelectorAll('.tabheader__item'),
         tabParent = document.querySelector('.tabheader__items');
@@ -172,9 +171,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-
-
-
     //Line
     const line = document.querySelector('.line');
 
@@ -183,7 +179,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function change() {
         line.style.width = `${Math.floor(document.documentElement.scrollTop / 2.9)}px`;
     }
-
 
     //Menu
     class Menu {
@@ -228,35 +223,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new Menu(
-        'img/tabs/vegy.jpg',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        7,
-        'image',
-        '[data-content]',
 
-    );
+    //Data get 
+    const dataGet = async url => await axios.get(url);
 
-    new Menu(
-        'img/tabs/elite.jpg',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        9,
-        'image',
-        '[data-content]',
+    dataGet('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({
+                img,
+                altming,
+                title,
+                descr,
+                price
+            }) => {
+                new Menu(
+                    img,
+                    title,
+                    descr,
+                    price,
+                    altming,
+                    '[data-content]'
+                );
+            });
+        });
 
-    );
-
-    new Menu(
-        'img/tabs/post.jpg',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное  количество белков за счет тофу и импортных вегетарианских стейков.!',
-        8,
-        'image',
-        '[data-content]',
-
-    );
+    const dataPost = async (url, body) => axios.post(url, body);
 
     //form
     const forms = document.querySelectorAll('form');
@@ -281,28 +272,19 @@ document.addEventListener('DOMContentLoaded', () => {
             form.insertAdjacentElement('afterend', requestMessage);
 
             const formData = new FormData(form);
-            const object = {};
-            formData.forEach((value, key) => {
-                object[key] = value;
-            });
 
-            fetch('server.php',{
-                method: "POST",
-                headers : {
-                    'Content-type': 'application/json'
-                 },
-                body : JSON.stringify(object)
-            })
-            .then(data => data.text())
-            .then(data => {
-                console.log(data);
-                requestModal(resultRequest.success);
-                requestMessage.remove();
-            })
-            .catch(() => {
-                requestModal(resultRequest.error);
-            })
-            .finally(() => form.reset());
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            dataPost('http://localhost:3000/requests', json)
+                .then(data => {
+                    console.log(data.data);
+                    requestModal(resultRequest.success);
+                    requestMessage.remove();
+                })
+                .catch(() => {
+                    requestModal(resultRequest.error);
+                })
+                .finally(() => form.reset());
         });
     }
 
@@ -330,6 +312,116 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
- 
+    //Slider
+    const slides = document.querySelectorAll('.offer__slide'),
+        count = document.querySelector('#current'),
+        countTotal = document.querySelector('#total'),
+        mainElementOfSlides = document.querySelector('.offer__slider-wrapper'),
+        slidesFields = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(mainElementOfSlides).width,
+        slider = document.querySelector('.offer__slider');
+
+    let index = 1,
+        offset = 0;
+
+    countTotal.textContent = helper(slides.length);
+    count.textContent = helper(index);
+
+    slidesFields.style.width = 100 * slides.length + '%';
+    slidesFields.style.display = 'flex';
+    slidesFields.style.transition = '0.5s all';
+
+    mainElementOfSlides.style.overflow = 'hidden';
+
+    slides.forEach(value => value.style.width = width);
+
+    //Dots
+    slider.style.position = 'relative';
+    let dots = document.createElement('ol');
+    dots.classList.add('carousel-indicators');
+
+    let dotsList;
+
+    const addElement = new Promise((resolve,reject) => {
+        slides.forEach((value, index) => {
+            dots.innerHTML += `
+            <li class = "dot" data-content-to="${index + 1}"></li>
+         `;
+        });
+
+        resolve();
+    });
+
+    addElement.then(() => {
+        dotsList = document.querySelectorAll('.dot');
+        dotsList[index - 1].classList.add('active');
+    });
+
+    slider.addEventListener('click', (e) => {
+
+        if (e.target && e.target.matches('li.dot')) {
+
+            dotsList.forEach(value => {
+
+                value.classList.remove('active');
+
+                if (value === e.target) {
+                    value.classList.add('active');
+                    index = value.dataset.contentTo;
+                    offset = +width.slice(0, width.length - 2) * (value.dataset.contentTo - 1);
+                    slidesFields.style.transform = `translateX(-${offset}px)`;
+                    
+                    count.textContent = helper(value.dataset.contentTo);
+                }
+            });
+        }
+    });
+
+    slider.append(dots);
+
+    function addActive() {
+        dotsList.forEach(value => value.classList.remove('active'));
+        dotsList[index - 1].classList.add('active');
+    }
+
+    document.querySelector('.offer__slider-next').addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesFields.style.transform = `translateX(-${offset}px)`;
+
+        if (index === slides.length) {
+            index = 1;
+        } else {
+            index++;
+        }
+
+        count.textContent = helper(index);
+
+        addActive();
+    });
+
+    document.querySelector('.offer__slider-prev').addEventListener('click', () => {
+        if (offset === 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesFields.style.transform = `translateX(-${offset}px)`;
+
+        if (index === 1) {
+            index = slides.length;
+        } else {
+            index--;
+        }
+
+        count.textContent = helper(index);
+        
+        addActive();
+    });
+
+
 
 });
